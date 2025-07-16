8be398783ebe768e0897ec6bb28c305fd8fe64ed
AntonyHz <hernandezantony333@gmail.com>
2025-07-15 22:25:48 -0400

    Agregando funcion addItem

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,15 +1,30 @@
 import { useState } from "react"
-import type { OrderItem } from "../types";
+import type { MenuItem, OrderItem } from "../types";
 
 export default function useOrder(){
 
     const [order, setOrder] = useState<OrderItem[]>([]);
 
-    console.log(order);
+    function addItem(item : MenuItem){
 
-    return{
+        const itemExists = order.findIndex(e => e.id === item.id);
 
-    }
+        if(itemExists >= 0){
 
+            // Ya existe
+            const orderUpdate = [...order];
+            orderUpdate[itemExists].quantity++;
+            setOrder(orderUpdate);
+        }else{
+
+            // No existe
+            const newItem : OrderItem = {...item, quantity : 1}
+            setOrder([...order, newItem]);            
+        };
+    };
     
+    console.log(order);
+    return{
+        addItem
+    }   
 }
