8dcbbfe5fb3a152a83edd42cc051e905d613e2d6
Antonyhz <hernandezantony333@gmail.com>
2025-07-17 16:50:44 -0400

    creando hook de order

---
+++ src/components/OrderContents.tsx
@@ -0,0 +1,28 @@
+import type { OrderItem } from "../types";
+
+type orderContentsProps = {
+    order: OrderItem[]
+};
+
+export default function OrderContents({order} : orderContentsProps){
+
+    return(
+        <>
+            <div>
+                <h2 className="font-black text-4xl">Consumo</h2>
+
+                <div className="space-y-3 mt-5">
+                    {order.length === 0 
+                    ? <p>La orden está vacia</p>
+                    : (
+                        order.map(item => (
+                            <div key={item.id}>
+                                <p>{item.name}</p>
+                            </div>
+                        ))
+                    )}
+                </div>
+            </div>
+        </>
+    );
+};
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -23,8 +23,8 @@ export default function useOrder(){
         };
     };
     
-    console.log(order);
     return{
-        addItem
+        order,
+        addItem,
     }   
 }
