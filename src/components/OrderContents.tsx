3323784d8387ea28c4d2a9621db5e281fc73e4b2
AntonyHz <hernandezantony333@gmail.com>
2025-07-18 21:56:56 -0400

    estilos a la orden

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,4 +1,5 @@
 import type { OrderItem } from "../types";
+import { formatCurrency } from "../helpers";
 
 type orderContentsProps = {
     order: OrderItem[]
@@ -11,20 +12,27 @@ export default function OrderContents({order} : orderContentsProps){
             <div>
                 <h2 className="font-black text-4xl">Consumo</h2>
 
-                <div className="space-y-3 mt-5">
+                <div className="space-y-3 mt-10">
                     {order.length === 0 
                     ? <p>La orden está vacia</p>
                     : (
                         order.map(item => (
-                            <div key={item.id} className="flex items-center justify-between py-2 border-t-2 border-gray-200">
+                            <div 
+                                key={item.id} 
+                                className="flex items-center justify-between py-5 border-t border-gray-200 last-of-type:border-b"
+                            >
                                 <div>
-                                    <p>{item.name} - {item.price}</p>
-                                    <p>Cantidad: {item.quantity}</p>
-                                    <p>Total: {item.quantity * item.price}</p>
+                                    <p className="text-lg">
+                                        {item.name} - {formatCurrency(item.price)}
+                                    </p>
+
+                                    <p className="font-black">
+                                        Cantidad: {item.quantity} - {formatCurrency(item.quantity * item.price)}
+                                    </p>
                                 </div>
 
                                 <div>
-                                    <button className="bg-red-600 text-white h-8 w-8 rounded-4xl font-bold cursor-pointer">
+                                    <button className="bg-red-600 text-white h-8 w-8 rounded-full font-black cursor-pointer">
                                         X
                                     </button>
                                 </div>
